d9221f263b22f13affe1972a5cef27577f867dd7
wangyonghong <yonghongwang@163.com>
2020-07-29 10:23:57 +0800

    update

--- actions-build.sh
+++ actions-build.sh
@@ -2,7 +2,11 @@
 
 set -eux
 
-dockerfiles=`git log --name-only -1 | grep Dockerfile`
+dockerfiles=$(git log --name-only -1 | grep Dockerfile || true)
+
+if [[ ! -n "$dockerfiles" ]]; then
+    exit 0
+fi
 
 for dockerfile in ${dockerfiles[@]}; do
     echo "start build: "$dockerfile
